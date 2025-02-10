f56c9b54c7157d0e32e8d35924684a0f1524d78e
Avinash Chouhan <70231124+chouhan-avinash@users.noreply.github.com>
2025-02-10 15:35:02 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+import os
+import torchvision
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+
+filepath = "" //provide model path
+model = torch.load(filepath)
+print('MODEL LOADED')
+//run inference on input images using the loaded model.
